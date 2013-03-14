f3efc15f5743f53ad8d1261be9930ef3a60598f1
cmsbuild <>
2013-03-14 13:39:32 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- OfflineValidation-V02-10-02-10'.
    Sprout from master 2013-03-14 13:39:30 UTC Marco Musich <sha1-30be28a84c1b5db819409643e069569326ecef61@cern.ch> 'new version of submission script'
    Cherrypick from master 2012-12-04 17:05:49 UTC Devdatta Majumder <sha1-6e5890d5905a8cd26bf216b62ceb084835ffccd9@cern.ch> 'Fixing TrackerGeometryCompare.cc| for 53X':
        plugins/ResidualRefitting.cc
        plugins/ResidualRefitting.h
        plugins/TrackerGeometryCompare.cc
        plugins/TrackerGeometryCompare.h
        plugins/TrackerOfflineValidationSummary.cc
        plugins/ValidationMisalignedTracker.cc
        test/testCompare_cfg.py
        test/testIntoNtuples_cfg.py
    Cherrypick from master 2013-03-01 16:13:47 UTC Gregor Hellwig <sha1-0fd33106e71082062498fd3bb4060f6add1c2210@cern.ch> 'Further removal of old configuration remnants.':
        python/TkAlAllInOneTool/betterConfigParser.py
        python/TkAlAllInOneTool/genericValidation.py
        python/TkAlAllInOneTool/geometryComparison.py
        python/TkAlAllInOneTool/monteCarloValidation.py
        scripts/validateAlignments.py
    Cherrypick from master 2012-09-28 20:46:43 UTC Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch> 'call Sumw2() for profiles that are filled with weights to get meaningfull error bars (releative to rev. 1.52)':
        plugins/TrackerOfflineValidation.cc
    Cherrypick from master 2012-11-05 10:58:06 UTC Devdatta Majumder <sha1-6e5890d5905a8cd26bf216b62ceb084835ffccd9@cern.ch> 'Including surface deformations in Geometry comparison module.':
        plugins/TrackerGeometryIntoNtuples.cc
    Delete:
        doc/OfflineValidation.doc

--- python/TkAlAllInOneTool/betterConfigParser.py
+++ python/TkAlAllInOneTool/betterConfigParser.py
@@ -1,55 +1,8 @@
 import ConfigParser
 import os
-import copy
 from TkAlExceptions import AllInOneError
 
-
-class AdaptedDict(dict):
-    """
-    Dictionary which handles updates of values for already existing keys
-    in a modified way.
-    Instead of replacing the old value, the new value is appended to the
-    value string separated by `self.getSep()`.
-    This dictionary is used in the class `BetterConfigParser` instead of the
-    default `dict_type` of the `ConfigParser` class.
-    """
-
-    def getSep(self):
-        """
-        This method returns the separator used to separate the values for 
-        duplicate options in a config.
-        """
-        return " |/| "
-
-    def __setitem__(self, key, value, dict_setitem=dict.__setitem__):
-        """
-        od.__setitem__(i, y) <==> od[i]=y
-        Updating an existing key appends the new value to the old value
-        separated by `self.getSep()` instead of replacing it.
-
-        Arguments:
-        - `key`: key part of the key-value pair
-        - `value`: value part of the key-value pair
-        - `dict_item`: method which is used for finally setting the item
-        """
-
-        if "__name__" in self and self["__name__"]=="validation" and key in self:
-            the_value = self[key]+self.getSep()+value
-        else:
-            the_value = value
-        dict_setitem(self, key, the_value)
-
-
 class BetterConfigParser(ConfigParser.ConfigParser):
-    def __init__(self):
-        ConfigParser.ConfigParser.__init__(self,dict_type=AdaptedDict)
-        dummyDict = AdaptedDict()
-        self._sep = dummyDict.getSep()
-        del dummyDict
-
-    def getSep(self):
-        return self._sep
-
     def optionxform(self, optionstr):
         return optionstr
     
@@ -73,13 +26,13 @@ class BetterConfigParser(ConfigParser.ConfigParser):
                     result[option] = self.get( "local"+section.title(),
                                                    option )
         except ConfigParser.NoSectionError, section:
-            msg = ("%s in configuration files. This section is mandatory."
-                   %(str(section).replace(":", "", 1)))
-            raise AllInOneError(msg)
+            raise AllInOneError, ("%s in configuration files. This section is "
+                                  "mandatory."
+                                  %( str( section ).replace( ":", "", 1 ) ) )
         return result
 
     def getResultingSection( self, section, defaultDict = {}, demandPars = [] ):
-        result = copy.deepcopy(defaultDict)
+        result = defaultDict
         for option in demandPars:
             try:
                 result[option] = self.get( section, option )
@@ -95,15 +48,19 @@ class BetterConfigParser(ConfigParser.ConfigParser):
                     try:
                         result[option] = self.get( localSection, option )
                     except ConfigParser.NoOptionError, option:
-                        msg = ("%s. This option is mandatory."
-                               %(str(option).replace(":", "", 1).replace(
-                                    "section",
-                                    "section '"+globalSection+"' or", 1)))
-                        raise AllInOneError(msg)
+                        raise AllInOneError, ("%s. This option is mandatory."
+                                              %( str( option )\
+                                                 .replace( ":", "", 1 )\
+                                                 .replace( "section", "section '"\
+                                                           +globalSection+"' or", 1 )
+                                                 )
+                                              )
                 else:
-                    msg = ("%s. This option is mandatory."
-                           %(str(globalSectionError).replace(":", "", 1)))
-                    raise AllInOneError(msg)
+                    raise AllInOneError, ("%s. This option is mandatory."
+                                          %( str( globalSectionError )\
+                                                 .replace( ":", "", 1 )
+                                             )
+                                          )
         result = self.__updateDict( result, section )
         return result
 
@@ -119,9 +76,6 @@ class BetterConfigParser(ConfigParser.ConfigParser):
         compares = {}
         for section in self.sections():
             if "compare:" in section:
-                self.checkInput(section,
-                                knownSimpleOptions = ["levels", "dbOutput",
-                                                      "jobmode"])
                 levels = self.get( section, "levels" )
                 dbOutput = self.get( section, "dbOutput" )
                 compares[section.split(":")[1]] = ( levels, dbOutput )
@@ -138,10 +92,8 @@ class BetterConfigParser(ConfigParser.ConfigParser):
         general = self.getResultingSection( "general", defaultDict = defaults )
         return general
     
-    def checkInput(self, section, knownSimpleOptions=[], knownKeywords=[],
-                   ignoreOptions=[]):
-        """
-        Method which checks, if the given options in `section` are in the
+    def checkInput(self, section, knownSimpleOptions=[], knownKeywords=[]):
+        """Method which checks, if the given options in `section` are in the
         list of `knownSimpleOptions` or match an item of `knownKeywords`.
         This is basically a check for typos and wrong parameters.
         
@@ -150,17 +102,13 @@ class BetterConfigParser(ConfigParser.ConfigParser):
         - `knownSimpleOptions`: List of allowed simple options in `section`.
         - `knownKeywords`: List of allowed keywords in `section`.
         """
-
+        
         for option in self.options( section ):
             if option in knownSimpleOptions:
                 continue
             elif option.split()[0] in knownKeywords:
                 continue
-            elif option in ignoreOptions:
-                print ("Ignoring option '%s' in section '[%s]'."
-                       %(option, section))
             else:
-                msg = ("Invalid or unknown parameter '%s' in section '%s'!"
-                       %(option, section))
-                raise AllInOneError(msg)
+                raise AllInOneError, ("Invalid or unknown parameter '%s' in "
+                                      "section '%s'!")%( option, section )
 
